90473ebd76f347a568b38fc8c4e614ac927b4fa9
Marcos Spomberg <marcosspomberg@gmail.com>
2022-06-15 17:57:29 -0400

    Add logo section to topbar and initial css

--- src/components/topbar/topbar.jsx
+++ src/components/topbar/topbar.jsx
@@ -3,12 +3,12 @@ import './topbar.scss'
 export default function Topbar() {
   return (
     <div className='topbar'>
-      <div className="wrapper">
-        <div className="left">
-
+      <div className='wrapper'>
+        <div className='left'>
+          <a href='#intro' className='logo'>logo</a>
         </div>
-        <div className="right">
-
+        <div className='right'>
+          Right
         </div>
       </div>
     </div>
