65a9e38b41076941d9193dcb2d47cf09afa49b61
Marcos Spomberg <marcosspomberg@gmail.com>
2022-06-14 22:05:00 -0400

    Add IDs to components

--- src/components/topbar/topbar.jsx
+++ src/components/topbar/topbar.jsx
@@ -3,7 +3,14 @@ import './topbar.scss'
 export default function Topbar() {
   return (
     <div className='topbar'>
-      
+      <div className="wrapper">
+        <div className="left">
+
+        </div>
+        <div className="right">
+
+        </div>
+      </div>
     </div>
   )
 }
