e94ac1daf9ce75e5c15e4e54b485d411b786ca7d
Marcos Spomberg <marcosspomberg@gmail.com>
2022-06-16 17:52:49 -0400

    add phone and email icons and info  to topbar

--- src/components/topbar/topbar.jsx
+++ src/components/topbar/topbar.jsx
@@ -1,4 +1,5 @@
 import './topbar.scss'
+import { Email, Phone } from '@material-ui/icons';
 
 export default function Topbar() {
   return (
@@ -6,9 +7,17 @@ export default function Topbar() {
       <div className='wrapper'>
         <div className='left'>
           <a href='#intro' className='logo'>logo</a>
+          <div className='itemContainer'>
+            <Email/>
+            <span>marcosspomberg@gmail.com</span>
+          </div>
+          <div className='itemContainer'>
+            <Phone/>
+            <span>647-807-9640</span>
+          </div>
         </div>
         <div className='right'>
-          Right
+          
         </div>
       </div>
     </div>
