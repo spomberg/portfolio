29e3967babf1a8e668584a6cd21db4c49cb120af
Marcos Spomberg <marcosspomberg@gmail.com>
2022-06-17 21:03:24 -0400

    Add menu component

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,8 @@ import Intro from "./components/intro/Intro";
 import Portfolio from "./components/portfolio/Portfolio";
 import Works from "./components/works/Works";
 import Contact from "./components/contact/Contact";
+import Menu from "./components/menu/Menu"
+
 import "./app.scss"
 import { useState } from "react";
 
@@ -12,6 +14,7 @@ function App() {
   return (
     <div className="app">
       <Topbar menuOpen={menuOpen} setMenuOpen={setMenuOpen}/>
+      <Menu menuOpen={menuOpen} setMenuOpen={setMenuOpen}/>
       <div className="sections">
         <Intro/>
         <Portfolio/>
