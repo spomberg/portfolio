70959f39f2dde72418d2fa4bc943677986e2fdc6
Marcos Spomberg <marcosspomberg@gmail.com>
2022-06-18 17:58:53 -0400

    Create left and right sections and add profile photo

--- src/components/intro/intro.jsx
+++ src/components/intro/intro.jsx
@@ -1,9 +1,17 @@
 import './intro.scss'
+import { KeyboardArrowDown } from '@material-ui/icons';
 
 export default function Intro() {
   return (
     <div className='intro' id='intro'>
-      
+      <div className='left'>
+        <div className='imgContainer'>
+          <img src="assets/profile-image.png" alt="" />
+        </div>
+      </div>
+      <div className='right'>
+
+      </div>
     </div>
   )
 }
