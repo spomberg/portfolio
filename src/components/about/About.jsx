2c8552a285428b33f5c8301586cc339de0b37967
Marcos Spomberg <marcosspomberg@gmail.com>
2022-06-19 19:22:57 -0400

    Add skill icons to assets folder

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -1,9 +1,25 @@
 import './about.scss';
+import { aboutMe } from '../../data';
+import photo from '../../assets/images/photo.jpg'
 
-export default function AboutMe() {
+export default function About() {
   return (
-    <div className="About" id="about">
-      
+    <div className="about" id="about">
+      <div className="wrapper">
+        <div className="left">
+          <img src={photo} alt="Marcos" />
+        </div>
+        <div className="right">
+          <div className="aboutMeText">
+            <h2>About Me</h2>
+            <span>{aboutMe.text}</span>
+          </div>
+          <div className="aboutMeSkills">
+            <h2>Skills</h2>
+            <span>{aboutMe.skills}</span>
+          </div>
+        </div>
+      </div>
     </div>
   )
 }
