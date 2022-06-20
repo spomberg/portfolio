4f43b1248cae526dd803431bbcd49ac49448e19e
Marcos Spomberg <marcosspomberg@gmail.com>
2022-06-19 22:07:54 -0400

    Add aditional skill icons

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -3,6 +3,10 @@ import { aboutMe } from '../../data';
 import photo from '../../assets/images/photo.jpg';
 import javascript from '../../assets/images/javascript-icon.png';
 import html from '../../assets/images/html5-icon.png';
+import sass from '../../assets/images/sass.png';
+import react from '../../assets/images/react-js.png';
+import sql from '../../assets/images/sql.png';
+import ruby from '../../assets/images/ruby-on-rails.png';
 
 export default function About() {
   return (
@@ -23,6 +27,10 @@ export default function About() {
           <div className="skillIcons">
             <img src={javascript} alt="javascript icon" />
             <img src={html} alt="html icon" />
+            <img src={sass} alt="sass icon" />
+            <img src={react} alt="react icon" />
+            <img src={sql} alt="sql icon" />
+            <img src={ruby} alt="ruby icon" />
           </div>
         </div>
       </div>
