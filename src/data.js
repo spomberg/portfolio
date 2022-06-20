12d355c374ac9e9f027829c5d7a36abe6e5bd11e
Marcos Spomberg <marcosspomberg@gmail.com>
2022-06-20 19:16:57 -0400

    Add works data

--- src/data.js
+++ src/data.js
@@ -3,6 +3,44 @@ export const aboutMe = {
   skills: "JavaScript, HTML5, CSS, SASS, React.JS, Node.JS, Ruby/Rails, JQuery, Ajax, PostgreSQL, Git, Bootstrap, Jest, Cypress, Storybook"
 }
 
+export const portfolio = [
+  {
+    id: 1,
+    title: "Trash2Treasure",
+    description: "Platform for users to list items (treasures) that are being left for donation in public spaces. Created with React and Ruby on Rails.",
+    img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRKvdHn8GmPGCO0y3SJqNHACygpm0h9VycMHg&usqp=CAU"
+  },
+  {
+    id: 2,
+    title: "QuizApp",
+    description: "An application to create and share quizzes built with JavaScript, HTML5, Node, Express.",
+    img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRKvdHn8GmPGCO0y3SJqNHACygpm0h9VycMHg&usqp=CAU"
+  },
+  {
+    id: 3,
+    title: "Jungle",
+    description: "A mini e-commerce application built with Ruby on Rails.",
+    img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRKvdHn8GmPGCO0y3SJqNHACygpm0h9VycMHg&usqp=CAU"
+  },
+  {
+    id: 4,
+    title: "Scheduler",
+    description: "Single-page app built with React that allows students to book interviews.",
+    img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRKvdHn8GmPGCO0y3SJqNHACygpm0h9VycMHg&usqp=CAU"
+  },
+  {
+    id: 5,
+    title: "LightBnB",
+    description: "A simple AirBnB clone created to get comfortable with databases and SQL.",
+    img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRKvdHn8GmPGCO0y3SJqNHACygpm0h9VycMHg&usqp=CAU"
+  },
+  {
+    id: 6,
+    title: "TinyApp",
+    description: "Full-stack web application built with Node and Express that allows users to shorten long URLs (à la bit.ly)."
+  }
+]
+
 export const featuredPortfolio = [
     {
       id: 1,
