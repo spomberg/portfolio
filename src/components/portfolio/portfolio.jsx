1963442c3d8bd625a34f93a882b1bb2f178572ae
Marcos Spomberg <marcosspomberg@gmail.com>
2022-06-20 20:11:07 -0400

    Add work list

--- src/components/portfolio/portfolio.jsx
+++ src/components/portfolio/portfolio.jsx
@@ -1,9 +1,24 @@
-import './portfolio.scss'
+import './portfolio.scss';
+import { portfolio } from '../../data';
+import Work from './Work';
 
 export default function Portfolio() {
   return (
     <div className="portfolio" id="portfolio">
-      
+      <h1>Portfolio</h1>
+      <div className='container'>
+        <ul className='worksList'>
+          {Object.values(portfolio).map(work => {
+            return (
+              <li key={work.id}>
+                <Work 
+                {...work}
+                />
+              </li>
+            )
+          })}
+        </ul>
+      </div>
     </div>
   )
 }
