52c02676f11149eafc2565ded6b9573f82de845c
Marcos Spomberg <marcosspomberg@gmail.com>
2022-06-22 19:15:49 -0400

    Add link, stacks and gif keys to data file structure

---
+++ src/data.jsx
@@ -0,0 +1,63 @@
+export const aboutMe = {
+  text: `I'm a full-stack junior web developer and a recent graduate of Lighthouse Labs. 
+  \nThroughout my career, I have honed my people skills in other industries where I successfully lead projects in different areas and headed a 10 people team for 5 years. I also trained hundreds of people, as passing down knowledge (while continuously learning) is a passion of mine. 
+  \nMy goal is to now use my skills to make meaningful connections, exchange knowledge and help others while working as a Web Developer.`,
+  skills: "JavaScript, HTML5, CSS, SASS, React.JS, Node.JS, Ruby/Rails, JQuery, Ajax, PostgreSQL, Git, Bootstrap, Jest, Cypress, Storybook"
+}
+
+export const portfolio = [
+  {
+    id: 1,
+    title: "Trash2Treasure",
+    description: "Platform for users to list items (treasures) that are being left for donation in public spaces. Created with React and Ruby on Rails.",
+    img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRKvdHn8GmPGCO0y3SJqNHACygpm0h9VycMHg&usqp=CAU",
+    gif: "",
+    link: "https://github.com/spomberg/trash2treasure",
+    stacks: []
+  },
+  {
+    id: 2,
+    title: "QuizApp",
+    description: "An application to create and share quizzes built with JavaScript, HTML5, Node, Express.",
+    img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRKvdHn8GmPGCO0y3SJqNHACygpm0h9VycMHg&usqp=CAU",
+    gif: "",
+    link: "https://github.com/spomberg/quizapp",
+    stacks: []
+  },
+  {
+    id: 3,
+    title: "Jungle",
+    description: "A mini e-commerce application built with Ruby on Rails.",
+    img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRKvdHn8GmPGCO0y3SJqNHACygpm0h9VycMHg&usqp=CAU",
+    gif: "",
+    link: "https://github.com/spomberg/jungle-rails",
+    stacks: []
+  },
+  {
+    id: 4,
+    title: "Scheduler",
+    description: "Single-page app built with React that allows students to book interviews.",
+    img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRKvdHn8GmPGCO0y3SJqNHACygpm0h9VycMHg&usqp=CAU",
+    gif: "",
+    link: "https://github.com/spomberg/scheduler",
+    stacks: []
+  },
+  {
+    id: 5,
+    title: "LightBnB",
+    description: "A simple AirBnB clone created to get comfortable with databases and SQL.",
+    img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRKvdHn8GmPGCO0y3SJqNHACygpm0h9VycMHg&usqp=CAU",
+    gif: "",
+    link: "https://github.com/spomberg/LightBnB",
+    stacks: []
+  },
+  {
+    id: 6,
+    title: "TinyApp",
+    description: "Full-stack web application built with Node and Express that allows users to shorten long URLs (à la bit.ly).",
+    img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRKvdHn8GmPGCO0y3SJqNHACygpm0h9VycMHg&usqp=CAU",
+    gif: "",
+    link: "https://github.com/spomberg/tinyapp",
+    stacks: []
+  }
+]
