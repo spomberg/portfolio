c0a7eb06c92f82248eb3fdd49339a602baa46344
Marcos Spomberg <marcosspomberg@gmail.com>
2022-06-24 19:12:30 -0400

    Implement modal with basic project info

--- src/components/portfolio/Work.jsx
+++ src/components/portfolio/Work.jsx
@@ -1,8 +1,27 @@
+import { useState } from "react";
+import Modal from "react-bootstrap/Modal";
+
 export default function Work(props) {
+  const [show, setShow] = useState(false);
+
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true);
+
   return (
-    <div className="work">
-      <img src={props.img} alt={props.title} />
-      <h3>{props.title}</h3>
-    </div>
+    <>
+      <div className="work" onClick={handleShow}>
+        <img src={props.img} alt={props.title} />
+        <h3>{props.title}</h3>
+      </div>
+
+        <Modal show={show} onHide={handleClose}>
+        <Modal.Header closeButton>
+          <Modal.Title>{props.title}</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>{props.description}</Modal.Body>
+        <Modal.Footer>
+        </Modal.Footer>
+      </Modal>
+    </>
   )
 }
