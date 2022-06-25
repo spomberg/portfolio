60ec3a8d189245afc200a701710cb39e85effa3e
Marcos Spomberg <marcosspomberg@gmail.com>
2022-06-24 20:10:12 -0400

    Add gif, github link and stack icons to each project modal

--- src/components/portfolio/Work.jsx
+++ src/components/portfolio/Work.jsx
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import Modal from "react-bootstrap/Modal";
+import { GitHub } from '@material-ui/icons';
 
 export default function Work(props) {
   const [show, setShow] = useState(false);
@@ -18,8 +19,24 @@ export default function Work(props) {
         <Modal.Header closeButton>
           <Modal.Title>{props.title}</Modal.Title>
         </Modal.Header>
-        <Modal.Body>{props.description}</Modal.Body>
+        <Modal.Body>
+          {props.description}
+          <a href={props.link} target="_blank" rel="noreferrer">
+            <GitHub/> Visit GitHub repo
+          </a>
+          <img src={props.gif} alt={`${props.title} usage gif`} />
+        </Modal.Body>
         <Modal.Footer>
+          <h4>Stacks</h4>
+          <ul className="stackList">
+            {Object.values(props.stacks).map(stack => {
+              return (
+                <li>
+                  <img src={stack} alt="stack logo" />
+                </li>
+              )
+            })}
+          </ul>
         </Modal.Footer>
       </Modal>
     </>
