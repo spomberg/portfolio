77062c226e6f786023f1bb8af0980634f90b5723
Marcos Spomberg <marcosspomberg@gmail.com>
2022-06-24 20:08:58 -0400

    Import stack icons to data.jsx and add them to each project stack array

--- src/data.jsx
+++ src/data.jsx
@@ -1,3 +1,12 @@
+import javascript from '../src/assets/images/javascript-icon.png';
+import html from '../src/assets/images/html5-icon.png';
+import sass from '../src/assets/images/sass.png';
+import react from '../src/assets/images/react-js.png';
+import sql from '../src/assets/images/sql.png';
+import ruby from '../src/assets/images/ruby-on-rails.png';
+import node from '../src/assets/images/nodejs.png';
+import css from '../src/assets/images/css.png';
+
 export const aboutMe = {
   text: `I'm a full-stack junior web developer and a recent graduate of Lighthouse Labs. 
   \nThroughout my career, I have honed my people skills in other industries where I successfully lead projects in different areas and headed a 10 people team for 5 years. I also trained hundreds of people, as passing down knowledge (while continuously learning) is a passion of mine. 
@@ -13,7 +22,7 @@ export const portfolio = [
     img: "https://res.cloudinary.com/djv3yhbok/image/upload/v1656024604/portfolio/search-results1_nfgzdg.png",
     gif: "",
     link: "https://github.com/spomberg/trash2treasure",
-    stacks: []
+    stacks: [react, sass, ruby]
   },
   {
     id: 2,
@@ -22,7 +31,7 @@ export const portfolio = [
     img: "https://res.cloudinary.com/djv3yhbok/image/upload/v1655950756/portfolio/quiz_attempt_tklaqb.png",
     gif: "",
     link: "https://github.com/spomberg/quizapp",
-    stacks: []
+    stacks: [javascript, html, sass, sql, node]
   },
   {
     id: 3,
@@ -31,16 +40,16 @@ export const portfolio = [
     img: "https://res.cloudinary.com/djv3yhbok/image/upload/v1656024779/portfolio/book_interview_gkwfxq.png",
     gif: "",
     link: "https://github.com/spomberg/scheduler",
-    stacks: []
+    stacks: [react]
   },
   {
     id: 4,
     title: "Jungle",
     description: "A mini e-commerce application built with Ruby on Rails.",
     img: "https://res.cloudinary.com/djv3yhbok/image/upload/v1656035197/portfolio/FireShot_Capture_047_-_Jungle_-_localhost_fllw0k.png",
-    gif: "",
+    gif: "https://res.cloudinary.com/djv3yhbok/image/upload/v1656035534/portfolio/Recording_2022-06-23_at_21.50.28_t5ckl7.gif",
     link: "https://github.com/spomberg/jungle-rails",
-    stacks: []
+    stacks: [ruby, html, css]
   },
   {
     id: 5,
@@ -49,7 +58,7 @@ export const portfolio = [
     img: "https://res.cloudinary.com/djv3yhbok/image/upload/v1656024869/portfolio/main-page_k5d50s.png",
     gif: "",
     link: "https://github.com/spomberg/LightBnB",
-    stacks: []
+    stacks: [javascript, sql, html, css]
   },
   {
     id: 6,
@@ -58,6 +67,6 @@ export const portfolio = [
     img: "https://res.cloudinary.com/djv3yhbok/image/upload/v1656024969/portfolio/shortURL-page_nnm0ch.png",
     gif: "",
     link: "https://github.com/spomberg/tinyapp",
-    stacks: []
+    stacks: [javascript, html, node]
   }
 ]
