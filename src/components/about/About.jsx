9beafa754d2a682df63610fdeb1a89a3391ee60a
Marcos Spomberg <marcosspomberg@gmail.com>
2022-06-25 01:07:06 -0400

    Remove photo and reorganize text boxes

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -1,13 +1,13 @@
 import './about.scss';
 import { aboutMe } from '../../data';
 import { KeyboardArrowDown, KeyboardArrowUp } from '@material-ui/icons';
-import photo from '../../assets/images/photo.jpg';
 import javascript from '../../assets/images/javascript-icon.png';
 import html from '../../assets/images/html5-icon.png';
 import sass from '../../assets/images/sass.png';
 import react from '../../assets/images/react-js.png';
 import sql from '../../assets/images/sql.png';
 import ruby from '../../assets/images/ruby-on-rails.png';
+import node from '../../assets/images/nodejs.png'
 
 export default function About() {
   return (
@@ -17,22 +17,22 @@ export default function About() {
         </a>
       <div className="wrapper">
         <div className="left">
-          <img src={photo} alt="Marcos" />
-        </div>
-        <div className="right">
           <div className="aboutMeText">
             <h2>About Me</h2>
             <span>{aboutMe.text}</span>
           </div>
+        </div>
+        <div className="right">
           <div className="aboutMeSkills">
             <h2>Skills</h2>
             <span>{aboutMe.skills}</span>
           <div className="skillIcons">
             <img src={javascript} alt="javascript icon" />
-            <img src={html} alt="html icon" />
-            <img src={sass} alt="sass icon" />
+            <img src={node} alt="node icon" />
             <img src={react} alt="react icon" />
             <img src={sql} alt="sql icon" />
+            <img src={html} alt="html icon" />
+            <img src={sass} alt="sass icon" />
             <img src={ruby} alt="ruby icon" />
           </div>
           </div>
