36800ead7fabcd9d66c8ca9eb64dd50d84d61bb2
Marcos Spomberg <marcosspomberg@gmail.com>
2022-06-24 21:07:49 -0400

    Make modal larger

--- src/components/portfolio/Work.jsx
+++ src/components/portfolio/Work.jsx
@@ -15,27 +15,32 @@ export default function Work(props) {
         <h3>{props.title}</h3>
       </div>
 
-        <Modal show={show} onHide={handleClose}>
-        <Modal.Header closeButton>
-          <Modal.Title>{props.title}</Modal.Title>
-        </Modal.Header>
-        <Modal.Body>
-          {props.description}
-          <a href={props.link} target="_blank" rel="noreferrer">
-            <GitHub/> Visit GitHub repo
-          </a>
-          <img src={props.gif} alt={`${props.title} usage gif`} />
-        </Modal.Body>
-        <Modal.Footer>
-          <h4>Stacks</h4>
-          <div className="stackList">
-            {Object.values(props.stacks).map(stack => {
-              return (
-                <img key={stack} src={stack} alt="stack logo" />
-              )
-            })}
-          </div>
-        </Modal.Footer>
+        <Modal 
+          show={show} 
+          onHide={handleClose} 
+          className="workModal"
+          size="lg"
+        >
+          <Modal.Header closeButton>
+            <Modal.Title>{props.title}</Modal.Title>
+          </Modal.Header>
+          <Modal.Body className="modalBody">
+            <img src={props.gif} alt={`${props.title} usage gif`} />
+            <span>{props.description}</span>
+            <a href={props.link} target="_blank" rel="noreferrer">
+              <GitHub/> Visit the repo
+            </a>
+          </Modal.Body>
+          <Modal.Footer className="modalFooter">
+            <h4>Stacks</h4>
+            <div className="stackList">
+              {Object.values(props.stacks).map(stack => {
+                return (
+                  <img key={stack} src={stack} alt="stack logo" />
+                )
+              })}
+            </div>
+          </Modal.Footer>
       </Modal>
     </>
   )
