8a0a73f585d507169da6274a157ea13cdb11a2e6
Marcos Spomberg <marcosspomberg@gmail.com>
2022-06-25 11:51:31 -0400

    Fix work component import lines

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -1,17 +1,17 @@
 import './portfolio.scss';
-import './work.scss';
+import Work from '../work/Work';
+import '../work/work.scss';
 import { portfolio } from '../../data';
 import { KeyboardArrowUp } from '@material-ui/icons';
-import Work from './Work';
 
 export default function Portfolio() {
   return (
     <div className="portfolio" id="portfolio">
       <a href="#about">
-          <KeyboardArrowUp className="upArrow" />
+          <KeyboardArrowUp className="up-arrow" />
         </a>
       <h1>Portfolio</h1>
-        <ul className='worksList'>
+        <ul>
           {Object.values(portfolio).map(work => {
             return (
               <li key={work.id}>
