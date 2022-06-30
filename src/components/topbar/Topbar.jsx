1cb902729adbc74b062a9679862399582539f83c
Marcos Spomberg <marcosspomberg@gmail.com>
2022-06-29 21:23:57 -0400

    Fix topbar display for < 1050px width

--- src/components/topbar/Topbar.jsx
+++ src/components/topbar/Topbar.jsx
@@ -7,31 +7,37 @@ export default function Topbar({ menuOpen, setMenuOpen }) {
       <div className='wrapper'>
         <div className='left'>
           <a href='#intro' className='logo'>MS</a>
-          <div className='item-container'>
-            <a href='https://github.com/spomberg' target="_blank" rel="noreferrer">
-              <GitHub className='topbar-icon'/>/spomberg
-            </a>
+          <div className='links'>
+            <div className='item-container'>
+              <a href='https://github.com/spomberg' target="_blank" rel="noreferrer">
+                <GitHub className='topbar-icon'/>
+                <span>/spomberg</span>
+              </a>
+            </div>
+            <div className='item-container'>
+              <a href='https://linkedin.com/in/marcos-spomberg' target="_blank" rel="noreferrer">
+                <LinkedIn className='topbar-icon linkedin'/>
+                <span>/marcos-spomberg</span>
+              </a>
+            </div>
           </div>
-          <div className='item-container'>
-            <a href='https://linkedin.com/in/marcos-spomberg' target="_blank" rel="noreferrer">
-              <LinkedIn className='topbar-icon'/>/marcos-spomberg
-            </a>
-          </div>
-          <div className='item-container'>
-            <a href='https://resume.creddle.io/resume/ficw6ujhr3l' target="_blank" rel="noreferrer">
-              <span className='topbar-icon'>CV</span>
-              Resume
-            </a>
-          </div>
-          <div className='item-container'>
-            <a href='mailto:marcosspomberg@gmail.com'>
-              <Email className='topbar-icon'/>
-              marcosspomberg@gmail.com
-            </a>
-          </div>
-          <div className='item-container'>
-            <Phone className='topbar-icon'/>
-            <span>647-807-9640</span>
+          <div className='contact-info'>
+            <div className='item-container'>
+              <a href='https://resume.creddle.io/resume/ficw6ujhr3l' target="_blank" rel="noreferrer">
+                <span className='topbar-icon'>CV</span>
+                <span className='resume'>Resume</span>
+              </a>
+            </div>
+            <div className='item-container'>
+              <a href='mailto:marcosspomberg@gmail.com'>
+                <Email className='topbar-icon'/>
+                <span>marcosspomberg@gmail.com</span> 
+              </a>
+            </div>
+            <div className='item-container'>
+              <Phone className='topbar-icon'/>
+              <span>647-807-9640</span>
+            </div>
           </div>
         </div>
         <div className='right'>
