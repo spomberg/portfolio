aa52660559d3fd511f9319ae624763cdaddbb052
Marcos Spomberg <marcosspomberg@gmail.com>
2023-04-12 17:01:20 -0400

    Remove TinyApp project and add Book Bazaar data

--- src/data.jsx
+++ src/data.jsx
@@ -9,6 +9,7 @@ import mongoDb from '../src/assets/images/mongodb.svg';
 import materialUi from '../src/assets/images/material-ui.svg';
 import typescript from '../src/assets/images/typescript.png';
 import bootstrap from '../src/assets/images/bootstrap.png';
+import aws from '../src/assets/images/amazon-web-services-logo.png';
 
 export const aboutMe = {
   text: `Throughout my career, I have honed my people skills in other industries where I successfully led projects in different areas and headed a 10 people team for 5 years. I also trained hundreds of people, as passing down knowledge (while continuously learning) is a passion of mine. 
@@ -20,15 +21,6 @@ export const aboutMe = {
 export const portfolio = [
   {
     id: 1,
-    title: "Trash2Treasure",
-    description: "Platform for users to list items (treasures) that are being left for donation in public spaces. Created with React and Ruby on Rails.",
-    img: "https://res.cloudinary.com/djv3yhbok/image/upload/v1656024604/portfolio/search-results1_nfgzdg.png",
-    gif: "https://res.cloudinary.com/djv3yhbok/image/upload/q_auto:low/v1656463840/portfolio/t2t-1_bzjb0o.webp",
-    link: "https://github.com/spomberg/trash2treasure",
-    stacks: [react, sass, ruby]
-  },
-  {
-    id: 2,
     title: "My Movie List",
     description: "Full-stack app for users to create, edit and share movie lists with their friends. Built with React, Ruby on Rails, MongoDB and the TMDB API.",
     img: "https://res.cloudinary.com/djv3yhbok/image/upload/v1660601973/portfolio/My_Movie_List_qz8hnm.png",
@@ -36,6 +28,15 @@ export const portfolio = [
     link: "https://github.com/spomberg/my-movie-list",
     siteUrl: "https://mymovielist.ca",
     stacks: [ruby, mongoDb, react, sass, html, materialUi ]
+  },
+  {
+    id: 2,
+    title: "Trash2Treasure",
+    description: "Platform for users to list items (treasures) that are being left for donation in public spaces. Created with React and Ruby on Rails.",
+    img: "https://res.cloudinary.com/djv3yhbok/image/upload/v1656024604/portfolio/search-results1_nfgzdg.png",
+    gif: "https://res.cloudinary.com/djv3yhbok/video/upload/q_auto:eco/v1681253261/Book_Bazaar_-_10_April_2023_uuwgre.webm",
+    link: "https://github.com/spomberg/trash2treasure",
+    stacks: [react, sass, ruby]
   },
   {
     id: 3,
@@ -59,20 +60,20 @@ export const portfolio = [
   },
   {
     id: 5,
+    title: "Book Bazaar",
+    description: "Virtual bookstore where users can add and buy books. Running on several AWS services.",
+    img: "https://res.cloudinary.com/djv3yhbok/image/upload/q_auto:good/v1681253783/Book_Bazaar_screenshot_nreqcl.png",
+    gif: "https://res.cloudinary.com/djv3yhbok/video/upload/q_auto:good/v1681253261/Book_Bazaar_-_10_April_2023_uuwgre.webm",
+    siteUrl: "http://mybookstore-20230408214730-hostingbucket-dev.s3-website-us-east-1.amazonaws.com",
+    stacks: [aws, react, javascript]
+  },
+  {
+    id: 6,
     title: "QuizApp",
     description: "An application to create and share quizzes built with JavaScript, HTML5, Node, Express.",
     img: "https://res.cloudinary.com/djv3yhbok/image/upload/v1655950756/portfolio/quiz_attempt_tklaqb.png",
     gif: "https://res.cloudinary.com/djv3yhbok/image/upload/q_auto:eco/v1656536225/portfolio/quizapp_w553f3.webp",
     link: "https://github.com/spomberg/quizapp",
     stacks: [javascript, html, sass, sql, node]
-  },
-  {
-    id: 6,
-    title: "TinyApp",
-    description: "Full-stack web application built with Node and Express that allows users to shorten long URLs (à la bit.ly).",
-    img: "https://res.cloudinary.com/djv3yhbok/image/upload/v1656024969/portfolio/shortURL-page_nnm0ch.png",
-    gif: "https://res.cloudinary.com/djv3yhbok/image/upload/v1656539121/portfolio/tinyapp_ltope1.webp",
-    link: "https://github.com/spomberg/tinyapp",
-    stacks: [javascript, html, bootstrap, node]
-  }
+  } 
 ]
